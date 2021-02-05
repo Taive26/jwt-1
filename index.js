3de4f66cc36e93392e0214eb8a38a4b55dd00ea7
Kaido24 <mumm.meelis@gmail.com>
2021-02-05 16:26:21 +0200

    final app

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ const PORT = process.env.SERVER_PORT || 3000;
 
 const app = createApp();
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+// npm install express ejs dotenv body-parser jsonwebtoken bcryptjs sqlite3 morgan cookie-parser
